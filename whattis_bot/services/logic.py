ea158cda0e676fceb726ac055366b0aeb4302e56
Icon Stone Mexico <ubymarkdev@gmail.com>
2026-04-10 17:07:44 -0600

    logica v1

--- whattis_bot/services/logic.py
+++ whattis_bot/services/logic.py
@@ -20,7 +20,7 @@ def procesar_mensaje (mensaje):
     if (any(p in mensaje for p in ["que", "qué"]) 
     and 
     any(p in mensaje for p in ["precio", "precios", "costo", "costos"])
-    ): return "Hola buen día! Gracias por contactar con Icon Stone, líder en recubrimientos de alta calidad y vanguardia. Estamos ubicados en Zapopan, Jalisco, ¿En qué podemos ayudarle?"
+    ): return "Para poderle dar la información del precio del material de forma precisa por medio de una asesora, por favor, compartenos tu nombre, el nombre de la empresa de la que nos contactas y con que te identificas más: diseñador, arquitecto, carpintero, marmolero, distribuidor, constructora, ingeniero, contratista. De esta manera te asignaremos una asesora experta en tu rama para que pueda darle seguimiento a tu solicitud"
     
     #if any (p in mensaje for p in ["precio", "medidas", "cotizar", "costo"]):
     #    return "Claro, para apoyarte mejor con la información del material o la cotización que requieres mediante un asesor, nos podrías regalar tu nombre y el nombre de la empresa de donde nos contactas si es que vienes de una.Te identificas como: constructora, arquitecto, transformador o cliente final.Esto para ponerte en contacto con uno de nuestros asesores. El te podrá compartir especificaciones, catálogo, precios y también realizar cotización del material que necesites."
@@ -33,17 +33,17 @@ def procesar_mensaje (mensaje):
     #if any (p in mensaje for p in ["colores", "modelos", "catalogo", "catálogo"]):
     #    return "Claro, una de nuestras asesoras puede brindarle nuestro catálogo del material que necesitas para que pueda conocer todos lo modelos que manejamos y ver si alguno es similar a lo que busca, por favor, compartenos tu nombre, el nombre de la empresa de la que nos contactas y con que te identificas más: diseñador, arquitecto, carpintero, marmolero, distribuidor, constructora, ingeniero, contratista. De esta manera te asignaremos una asesora experta en tu rama para que pueda darle seguimiento a tu solicitud"
     
-    if (any(p in mensaje for p in ["cuarzo"]) 
+    if (any(p in mensaje for p in ["cuarzo", "cuarso", "cuarzos"]) 
     and 
-    any(p in mensaje for p in ["negro", "blanco", "claro", "vetas", "este", "similar"])
+    any(p in mensaje for p in ["negro", "blanco", "claro", "vetas", "este", "similar", "como"])
     ): return "Claro, una de nuestras asesoras puede brindarle nuestro catálogo de cuarzo Silestone que necesitas para que pueda conocer todos lo modelos que manejamos y ver si alguno es similar a lo que busca, por favor, compartenos tu nombre, el nombre de la empresa de la que nos contactas y con que te identificas más: diseñador, arquitecto, carpintero, marmolero, distribuidor, constructora, ingeniero, contratista. De esta manera te asignaremos una asesora experta en tu rama para que pueda darle seguimiento a tu solicitud"
     
 
     #if any (p in mensaje for p in ["verde", "negra", "blanca","azul", "traslucida", "blanco", "claro"]) and "cuarzo" in mensaje:
      #   return "Claro, una de nuestras asesoras puede brindarle nuestro catálogo de cuarzos para que pueda conocer todos lo modelos que manejamos y ver si alguno es similar a lo que busca, por favor, compartenos tu nombre, el nombre de la empresa de la que nos contactas y con que te identificas más: diseñador, arquitecto, carpintero, marmolero, distribuidor, constructora, ingeniero, contratista. De esta manera te asignaremos una asesora experta en tu rama para que pueda darle seguimiento a tu solicitud"
 
-    if any (p in mensaje for p in ["precio", "costo", "medidas", "costos"]) and "granito" in mensaje :
-        return "Buenos días! claro, una de nuestras asesoras puede darle más información de la existencia del material que necesita. Para apoyarte mejor con la información de las medidas el material o la cotización que requieres mediante un asesor, nos podrías regalar tu nombre y el nombre de la empresa de donde nos contactas si es que vienes de una.Te identificas como: constructora, arquitecto, transformador o cliente final.Esto para ponerte en contacto con uno de nuestros asesores. El te podrá compartir especificaciones, catálogo, precios y también realizar cotización del material que necesites." 
+    #if any (p in mensaje for p in ["precio", "costo", "medidas", "costos"]) and "granito" in mensaje :
+    #    return "Buenos días! claro, una de nuestras asesoras puede darle más información de la existencia del material que necesita. Para apoyarte mejor con la información de las medidas el material o la cotización que requieres mediante un asesor, nos podrías regalar tu nombre y el nombre de la empresa de donde nos contactas si es que vienes de una.Te identificas como: constructora, arquitecto, transformador o cliente final.Esto para ponerte en contacto con uno de nuestros asesores. El te podrá compartir especificaciones, catálogo, precios y también realizar cotización del material que necesites." 
     
     if (any(p in mensaje for p in ["manejan", "hacen"]) 
     and 
@@ -53,12 +53,12 @@ def procesar_mensaje (mensaje):
     #if any (p in mensaje for p in ("envío","envio", "flete","entrega", "envios", "envíos")):
     #    return "Claro, se puede coordinar un envío, el flete iría por su cuenta"
     
-    if (any(p in mensaje for p in ["marmol"]) 
+    if (any(p in mensaje for p in ["marmol", "mármol"]) 
     and 
     any(p in mensaje for p in ["negro", "blanco", "claro", "vetas", "como", "este"])
     ): return "Claro, una de nuestras asesoras puede brindarle nuestro catálogo de mármol para que pueda conocer todos lo modelos que manejamos y ver si alguno es similar a lo que busca, por favor, compartenos tu nombre, el nombre de la empresa de la que nos contactas y con que te identificas más: diseñador, arquitecto, carpintero, marmolero, distribuidor, constructora, ingeniero, contratista. De esta manera te asignaremos una asesora experta en tu rama para que pueda darle seguimiento a tu solicitud"
     
-    if (any(p in mensaje for p in ["granito"]) 
+    if (any(p in mensaje for p in ["granito", "granitos"]) 
     and 
     any(p in mensaje for p in ["como", "este"])
     ): return "Claro, una de nuestras asesoras puede brindarle nuestro catálogo de granitos para que pueda conocer todos lo modelos que manejamos y ver si alguno es similar a lo que busca, por favor, compartenos tu nombre, el nombre de la empresa de la que nos contactas y con que te identificas más: diseñador, arquitecto, carpintero, marmolero, distribuidor, constructora, ingeniero, contratista. De esta manera te asignaremos una asesora experta en tu rama para que pueda darle seguimiento a tu solicitud"
@@ -67,8 +67,8 @@ def procesar_mensaje (mensaje):
 
     if (any(p in mensaje for p in ["hacen", "realizan"]) 
     and 
-    any(p in mensaje for p in ["corte", "a medida"])
-    ): return "Hola buen día! Gracias por contactar con Icon Stone, líder en recubrimientos de alta calidad y vanguardia. Estamos ubicados en Zapopan, Jalisco, ¿En qué podemos ayudarle?"
+    any(p in mensaje for p in ["corte", "a medida", "cortes"])
+    ): return "Únicamente vendemos el material, sin embargo en el AMG podemos recomendarle algún transformador o instalador"
 
 
     #if any (p in mensaje for p in ["haces", "realizas"]) and "cortes" in mensaje:
@@ -79,4 +79,4 @@ def procesar_mensaje (mensaje):
     any(p in mensaje for p in ["fachada", "cotizar", "metros", "vetas", "como", "este"])
     ): return "Claro, una de nuestras asesoras puede brindarle nuestro catálogo de mármol que necesitas para que pueda conocer todos lo modelos que manejamos y ver si alguno es similar a lo que busca, por favor, compartenos tu nombre, el nombre de la empresa de la que nos contactas y con que te identificas más: diseñador, arquitecto, carpintero, marmolero, distribuidor, constructora, ingeniero, contratista. De esta manera te asignaremos una asesora experta en tu rama para que pueda darle seguimiento a tu solicitud"
 
-    return "Gracias por tu mensaje 😊 ¿Podrías darme más detalles para ayudarte mejor?"
+    return "No entendí la pregunta xd"
